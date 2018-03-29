cf914b0bc66391d5cbe95850b16257d103069bd4
Laura Schneider <laurie.schneider@gmail.com>
2018-03-29 20:19:30 +0200

    sql updated

--- sql/finalproject.sql
+++ sql/finalproject.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS images;
 DROP TABLE IF EXISTS scrapbook;
+DROP TABLE IF EXISTS pages;
 
 CREATE TABLE users (
         id SERIAL PRIMARY KEY,
@@ -13,19 +14,26 @@ CREATE TABLE users (
 
 CREATE TABLE images (
         id SERIAL PRIMARY KEY,
-        user_id INTEGER,
+        page_id INTEGER NOT NULL,
         url VARCHAR (255),
         description VARCHAR (255),
-        imagetitle VARCHAR (255),
+        image_title VARCHAR (255),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
 CREATE TABLE scrapbook (
         id SERIAL PRIMARY KEY,
-        user_id INTEGER,
+        user_id INTEGER NOT NULL,
         theme VARCHAR (255),
         size VARCHAR (255),
         color VARCHAR (255),
-        pagetitle VARCHAR (255),
+        scrapbook_title VARCHAR (255),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE pages (
+        id SERIAL PRIMARY KEY,
+        scrapbook_id INTEGER NOT NULL,
+        header VARCHAR (255),
+        description TEXT
     );
