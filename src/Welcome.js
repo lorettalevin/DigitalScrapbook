167ca0d5e002f54b226abc7b48f8c39b4860e2e6
lorettalevin <lorettalevin4@gmail.com>
2018-03-30 22:11:47 +0200

    fixed welcome file

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,13 +3,9 @@ import Registration from './registration'
 import Login from './login'
 import {HashRouter, Route} from 'react-router-dom';
 
-
-
 export default function Welcome(props) {
-    console.log(location.pathname);
     return (
         <div>
-            <h1>welcome</h1>
             <HashRouter>
                 <div>
                     <Route exact path="/" component={Registration}/>
